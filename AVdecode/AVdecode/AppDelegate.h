289373bd3832a430ab38e764d4f325cc80833829
hbj <hbj@hbjtekiMac-mini.local>
2017-08-15 17:34:13 +0800

    decode-master

---
+++ AVdecode/AVdecode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVdecode
+//
+//  Created by hbj on 2017/8/15.
+//  Copyright © 2017年 宝剑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVdecode/AVdecode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVdecode
+//
+//  Created by hbj on 2017/8/15.
+//  Copyright © 2017年 宝剑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
